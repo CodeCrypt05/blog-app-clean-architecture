8cccb7dc433f1694a448dc0315d56e32bddafc29
vaibha-home <vaibhavwaghre@gmail.com>
2024-03-18 22:14:39 +0530

    dependency injection and get_it implemented

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,6 +1,4 @@
 import 'package:blog_app/core/error/exceptions.dart';
-import 'package:blog_app/core/error/failure.dart';
-import 'package:fpdart/fpdart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
@@ -19,6 +17,7 @@ abstract interface class AuthRemoteDataSource {
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   final SupabaseClient supabaseClient;
   AuthRemoteDataSourceImpl(this.supabaseClient);
+
   @override
   Future<String> signInWithEmailPassword({
     required String email,
@@ -35,12 +34,13 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
     required String password,
   }) async {
     try {
-      final response = await supabaseClient.auth
-          .signUp(password: password, email: email, data: {
-        'name': name,
-      });
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {'name': name},
+      );
       if (response.user == null) {
-        throw const ServerException('User is null!');
+        throw const ServerException("User is null");
       }
       return response.user!.id;
     } catch (e) {
